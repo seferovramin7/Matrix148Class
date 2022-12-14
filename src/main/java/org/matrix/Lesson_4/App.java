d130456fb8f7e83be99bbd26f16f1ae3cf7b4d3a
seferovramin7 <seferovramin7@gmail.com>
2022-12-14 20:40:22 +0400

    Lesson 4

--- src/main/java/org/matrix/Lesson_4/App.java
+++ src/main/java/org/matrix/Lesson_4/App.java
@@ -6,6 +6,11 @@ package org.matrix.Lesson_4;
 public class App {
     public static void main(String[] args) {
 
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Salam " + i);
+        }
+
+
         int pul = 0;
         do {
             System.out.println("Corek ver");
