c9ab942cc4337c0dc0f9df7597af9b750f4254f7
seferovramin7 <seferovramin7@gmail.com>
2022-12-14 20:24:55 +0400

    Lesson 4

--- src/main/java/org/matrix/Lesson_4/App.java
+++ src/main/java/org/matrix/Lesson_4/App.java
@@ -2,12 +2,19 @@ package org.matrix.Lesson_4;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello Matrix!" );
+public class App {
+    public static void main(String[] args) {
+        int a = 10;
+        boolean pulumVarmi = true;
+        while (pulumVarmi) {
+            System.out.println("Miqdar " + a);
+            a = a - 1;
+            if (a == 0) {
+                pulumVarmi = false;
+            }
+        }
+
+
     }
 }
