8e33972fe2dea9da58115c45579df1ccbc10c236
seferovramin7 <seferovramin7@gmail.com>
2022-12-21 21:09:22 +0400

    Lesson 6

--- src/main/java/org/matrix/Lesson_6/MahinFabriki.java
+++ src/main/java/org/matrix/Lesson_6/MahinFabriki.java
@@ -2,7 +2,12 @@ package org.matrix.Lesson_6;
 
 public class MahinFabriki {
 
+    int b;
+    static int c;
+
     public static void main(String[] args) {
+
+        int a;
 
         SedanMashin skodaMashini = new SedanMashin();
         skodaMashini.setTypeOfEngine("V16");
@@ -19,6 +24,17 @@ public class MahinFabriki {
         scania.setTypeOfVehicle("Truck");
         scania.setVolumeOfEngine(12);
 
-    }
+        System.out.println(toyota.getVolumeOfEngine());
 
+
+        SedanMashin volvo = new SedanMashin("SUV", "V8", 2);
+        System.out.println(volvo);
+
+        SedanMashin jeep = new SedanMashin("SUV", "V8");
+
+        SedanMashin gmc = new SedanMashin("SUV");
+
+
+
+    }
 }
--- src/main/java/org/matrix/Lesson_6/SedanMashin.java
+++ src/main/java/org/matrix/Lesson_6/SedanMashin.java
@@ -6,20 +6,52 @@ public class SedanMashin {
     String typeOfEngine;
     int volumeOfEngine;
 
+    public SedanMashin(String typeOfVehicle, String typeOfEngine, int volumeOfEngine) {
+        this.typeOfVehicle = typeOfVehicle;
+        this.typeOfEngine = typeOfEngine;
+        this.volumeOfEngine = volumeOfEngine;
+    }
+
+    public SedanMashin(String typeOfVehicle, String typeOfEngine) {
+        this.typeOfVehicle = typeOfVehicle;
+        this.typeOfEngine = typeOfEngine;
+    }
+
+    public SedanMashin(String typeOfVehicle) {
+        this.typeOfVehicle = typeOfVehicle;
+    }
+
+    public SedanMashin() {
+    }
+
+    public String getTypeOfVehicle() {
+        return typeOfVehicle;
+    }
+
     public void setTypeOfVehicle(String typeOfVehicle) {
         this.typeOfVehicle = typeOfVehicle;
+    }
+
+    public String getTypeOfEngine() {
+        return typeOfEngine;
     }
 
     public void setTypeOfEngine(String typeOfEngine) {
         this.typeOfEngine = typeOfEngine;
     }
 
+    public int getVolumeOfEngine() {
+        return volumeOfEngine;
+    }
+
     public void setVolumeOfEngine(int volumeOfEngine) {
         this.volumeOfEngine = volumeOfEngine;
     }
 
-
-
-
-
+    @Override
+    public String toString() {
+        return "tip " + typeOfVehicle + "\n"  +
+                "motor " + typeOfEngine + "\n" +
+                "hecm " + volumeOfEngine ;
+    }
 }
